7e5d843d6377464956755bae09e75de09750354f
douevenfeel <mdxnilovd@gmail.com>
2022-09-20 16:55:22 +0300

    init && lab1

---
+++ polls/views.py
@@ -0,0 +1,21 @@
+from datetime import date
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def getItems(request):
+    return render(request, 'items.html',{'data':{
+        'date':date.today(),
+        'items':[
+            {'id':1, 'title':'Iphone 11', 'price': 49900},
+            {'id':2, 'title':'Iphone 12', 'price': 54900},
+            {'id':3, 'title':'Iphone 13', 'price': 59900},
+        ]
+    }})
+
+def getItem(request, id, title = 'none', price = 0):
+    return render(request,'itemInfo.html',{'data':{
+        'date': date.today(),
+        'id': id,
+        'title': title,
+        'price': price
+    }})
